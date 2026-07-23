b074ba5c60c5d84cd76b18b9475ec1d41f5bea75
Earth1283 <josephcui921@gmail.com>
2026-07-23 19:18:04 +0800

    Initial commit

---
+++ src/main/kotlin/io/github/Earth1283/playerCrasherPlus/PlayerCrasherPlus.kt
@@ -0,0 +1,14 @@
+package io.github.earth1283.playerCrasherPlus
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PlayerCrasherPlus : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
